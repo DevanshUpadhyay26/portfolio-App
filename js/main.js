1334d0c4f6ab3ccb7d8cfc372f21631891a6ede0
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2024-07-01 12:37:33 +0530

    added transcripts and header

--- js/main.js
+++ js/main.js
@@ -109,11 +109,11 @@ function changeTranscript(type) {
     var officialLink = document.getElementById('officialTranscriptLink');
 
     if (type === 'unofficial') {
-        iframe.src = '/assets/PDF/Aidan Andrews Unofficial Transcript.pdf';
+        iframe.src = '/assets/PDF/BTechMarksheet.pdf';
         unofficialLink.classList.add('active');
         officialLink.classList.remove('active');
     } else if (type === 'official') {
-        iframe.src = 'assets/PDF/Aidan Andrews Official Transcript.pdf';
+        iframe.src = '/assets/PDF/diploma Degree.pdf';
         unofficialLink.classList.remove('active');
         officialLink.classList.add('active');
     }
